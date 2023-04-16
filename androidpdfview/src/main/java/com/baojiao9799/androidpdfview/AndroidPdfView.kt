428807fece1b0605086acb12a5c7a462d8fcc906
baojiao9799 <41097137+baojiao9799@users.noreply.github.com>
2023-04-15 21:58:12 -0400

    add tap zoom support and work on panning

--- androidpdfview/src/main/java/com/baojiao9799/androidpdfview/AndroidPdfView.kt
+++ androidpdfview/src/main/java/com/baojiao9799/androidpdfview/AndroidPdfView.kt
@@ -4,13 +4,19 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.RectF
 import android.graphics.pdf.PdfRenderer
 import android.os.ParcelFileDescriptor
 import android.util.AttributeSet
+import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.MotionEvent
+import android.view.MotionEvent.INVALID_POINTER_ID
 import android.view.ScaleGestureDetector
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.MotionEventCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.baojiao9799.androidpdfview.databinding.AndroidPdfViewBinding
 import com.baojiao9799.androidpdfview.reyclerview.PageSpacer
@@ -34,11 +40,27 @@ class AndroidPdfView
     private var pageSpacing = 0
     private var showScrollBar = true
     private var supportPinchZoom = true
+    private var supportTapZoom = true
+
+    private val tapZoomMinScale = 1f
+    private val tapZoomMaxScale = 2f
 
     private var currentScaleFactor = 1f
+    // The current viewport. This rectangle represents the currently visible
+    // chart domain and range.
+    private val mCurrentViewport = RectF(
+        0f,
+        0f,
+        context.resources.displayMetrics.widthPixels.toFloat(),
+        context.resources.displayMetrics.heightPixels.toFloat()
+    )
 
-    private val zoomListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+    // The current destination rectangle (in pixel coordinates) into which the
+    // chart data should be drawn.
+    private val mContentRect: Rect? = null
 
+
+    private val pinchZoomListener = object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
         override fun onScale(detector: ScaleGestureDetector): Boolean {
             currentScaleFactor *= detector.scaleFactor
 
@@ -52,7 +74,49 @@ class AndroidPdfView
         }
     }
 
-    private val zoomDetector = ScaleGestureDetector(context, zoomListener)
+    private val doubleTapListener = object : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onDoubleTap(event: MotionEvent): Boolean {
+            currentScaleFactor = when {
+                currentScaleFactor >= tapZoomMaxScale -> {
+                    tapZoomMinScale
+                }
+                else -> {
+                    tapZoomMaxScale
+                }
+            }
+            invalidate()
+            return true
+        }
+
+        override fun onScroll(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            // Scrolling uses math based on the viewport (as opposed to math using pixels).
+
+            mContentRect?.apply {
+                // Pixel offset is the offset in screen pixels, while viewport offset is the
+                // offset within the current viewport.
+                val viewportOffsetX = distanceX * mCurrentViewport.width() / width()
+                val viewportOffsetY = -distanceY * mCurrentViewport.height() / height()
+
+
+                // Updates the viewport, refreshes the display.
+                setViewportBottomLeft(
+                    mCurrentViewport.left + viewportOffsetX,
+                    mCurrentViewport.bottom + viewportOffsetY
+                )
+            }
+
+            return true
+        }
+    }
+
+    private val pinchZoomDetector = ScaleGestureDetector(context, pinchZoomListener)
+    private val doubleTapDetector = GestureDetector(context, doubleTapListener)
 
     init {
         context.theme.obtainStyledAttributes(
@@ -80,6 +144,10 @@ class AndroidPdfView
                 )
                 supportPinchZoom = getBoolean(
                     R.styleable.AndroidPdfView_supportPinchZoom,
+                    true
+                )
+                supportTapZoom = getBoolean(
+                    R.styleable.AndroidPdfView_supportTapZoom,
                     true
                 )
             } finally {
@@ -113,7 +181,11 @@ class AndroidPdfView
 
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
         if (supportPinchZoom) {
-            zoomDetector.onTouchEvent(event)
+            pinchZoomDetector.onTouchEvent(event)
+        }
+
+        if (supportTapZoom) {
+            doubleTapDetector.onTouchEvent(event)
         }
 
         return super.dispatchTouchEvent(event)
@@ -172,5 +244,29 @@ class AndroidPdfView
         renderer.close()
 
         return pdfPages
+    }
+
+    /**
+     * Sets the current viewport (defined by mCurrentViewport) to the given
+     * X and Y positions. Note that the Y value represents the topmost pixel position,
+     * and thus the bottom of the mCurrentViewport rectangle.
+     */
+    private fun setViewportBottomLeft(x: Float, y: Float) {
+        /*
+         * Constrains within the scroll range. The scroll range is simply the viewport
+         * extremes (AXIS_X_MAX, etc.) minus the viewport size. For example, if the
+         * extremes were 0 and 10, and the viewport size was 2, the scroll range would
+         * be 0 to 8.
+         */
+
+        val curWidth: Float = mCurrentViewport.width()
+        val curHeight: Float = mCurrentViewport.height()
+        val newX: Float = Math.max(0f, Math.min(x, context.resources.displayMetrics.widthPixels - curWidth))
+        val newY: Float = Math.max(0f + curHeight, Math.min(y, context.resources.displayMetrics.heightPixels.toFloat()))
+
+        mCurrentViewport.set(newX, newY - curHeight, newX + curWidth, newY)
+
+        // Invalidates the View to update the display.
+        ViewCompat.postInvalidateOnAnimation(this)
     }
 }
